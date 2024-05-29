d4350f4fe3180931a316b6ea86793b22635f008d
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-05-29 16:48:21 -0300

    fixing: fixing user icon roundness problem

--- components/UserLogedIn.vue
+++ components/UserLogedIn.vue
@@ -2,8 +2,8 @@
 
     <nuxt-link to="">
         <div
-            class="user w-10 h-10 border-customWhite border-[1px] rounded-full flex justify-center hover:shadow-md hover:shadow-customBlack transition-all duration-600 ">
-            <Icon name="fa-regular:user" class="m-auto text-customWhite" />
+            class="user w-min h-min p-3 border-customWhite border-[1px] rounded-full flex justify-center hover:shadow-md hover:shadow-customBlack transition-all duration-600 ">
+            <Icon name="fa-regular:user" class="m-auto text-customWhite  " />
         </div>
     </nuxt-link>
 
