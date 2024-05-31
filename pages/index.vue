d46e2f69c940be2b65787aab20178c1106ca5d83
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-05-31 12:05:22 -0300

    add time subfilter

--- pages/index.vue
+++ pages/index.vue
@@ -74,6 +74,83 @@ const changeCurrent = (e) => {
                     <ul class="p-3 space-y-1 text-sm text-customBlack dark:text-customWhite"
                         aria-labelledby="dropdownHelperButton">
                         <li>
+
+
+                            <button id="timeButton" data-dropdown-toggle="timeFilter"
+                                class="text-customBlack bg-customGray hover:bg-customGray focus:ring-4 focus:outline-none focus:ring-customGray font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-customGray dark:hover:bg-customGray dark:focus:ring-customGray"
+                                type="button">filters <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg>
+                            </button>
+                            <!-- timer drop menu -->
+                            <div id="timeFilter"
+                                class="z-10 hidden bg-customGray divide-y divide-customGray rounded-lg shadow w-60 dark:bg-customBlack dark:divide-customBlack">
+                                <ul class="p-3 space-y-1 text-sm text-customBlack dark:text-customWhite"
+                                    aria-labelledby="timeButton">
+                                    <li>
+                                        <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
+                                            <div class="flex items-center h-5">
+                                                <input id="helper-checkbox-1" aria-describedby="helper-checkbox-text-1"
+                                                    type="checkbox" value=""
+                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            </div>
+                                            <div class="ms-2 text-sm">
+                                                <label for="helper-checkbox-1"
+                                                    class="font-medium text-gray-900 dark:text-gray-300">
+                                                    <div>Enable notifications</div>
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                    </li>
+                                    <li>
+                                        <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
+                                            <div class="flex items-center h-5">
+                                                <input id="helper-checkbox-1" aria-describedby="helper-checkbox-text-1"
+                                                    type="checkbox" value=""
+                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            </div>
+                                            <div class="ms-2 text-sm">
+                                                <label for="helper-checkbox-1"
+                                                    class="font-medium text-gray-900 dark:text-gray-300">
+                                                    <div>Enable notifications</div>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
+                                            <div class="flex items-center h-5">
+                                                <input id="helper-checkbox-1" aria-describedby="helper-checkbox-text-1"
+                                                    type="checkbox" value=""
+                                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            </div>
+                                            <div class="ms-2 text-sm">
+                                                <label for="helper-checkbox-1"
+                                                    class="font-medium text-gray-900 dark:text-gray-300">
+                                                    <div>Enable notifications</div>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </li>
+                        <li>
+                            <button id="ingredientButton" data-dropdown-toggle="ingredientFilter"
+                                class="text-customBlack bg-customGray hover:bg-customGray focus:ring-4 focus:outline-none focus:ring-customGray font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-customGray dark:hover:bg-customGray dark:focus:ring-customGray"
+                                type="button">filters <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg>
+                            </button>
+                        </li>
+                        <li>
                             <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
                                 <div class="flex items-center h-5">
                                     <input id="helper-checkbox-1" aria-describedby="helper-checkbox-text-1"
@@ -87,34 +164,7 @@ const changeCurrent = (e) => {
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
-                                <div class="flex items-center h-5">
-                                    <input id="helper-checkbox-2" aria-describedby="helper-checkbox-text-2"
-                                        type="checkbox" value=""
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                </div>
-                                <div class="ms-2 text-sm">
-                                    <label for="helper-checkbox-2" class="font-medium text-gray-900 dark:text-gray-300">
-                                        <div>Enable 2FA auth</div>
-                                    </label>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="flex p-2 rounded hover:bg-customWhite dark:hover:bg-gray-600">
-                                <div class="flex items-center h-5">
-                                    <input id="helper-checkbox-3" aria-describedby="helper-checkbox-text-3"
-                                        type="checkbox" value=""
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                </div>
-                                <div class="ms-2 text-sm">
-                                    <label for="helper-checkbox-3" class="font-medium text-gray-900 dark:text-gray-300">
-                                        <div>Subscribe newsletter</div>
-                                    </label>
-                                </div>
-                            </div>
-                        </li>
+
                     </ul>
                 </div>
 
