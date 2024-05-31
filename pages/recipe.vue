95090d257c445560354bfeffdaf8f95426af788e
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-05-31 19:43:28 -0300

    adding footer on recipe page

--- pages/recipe.vue
+++ pages/recipe.vue
@@ -5,6 +5,7 @@
         </NavBar>
         <ShowCase />
         <Details />
+        <Footer />
     </div>
 </template>
 
@@ -14,6 +15,7 @@ import NavBarVue from "~/components/NavBar.vue";
 import UserLogedIn from "~/components/UserLogedIn.vue";
 import ShowCase from "~/components/ShowCase.vue"
 import Details from "~/components/Details.vue";
+import Footer from "~/components/Footer.vue";
 
 </script>
 
