a6e69a9c24c45b9d7ef2fa6571cc1e0b8dc3a109
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-06-01 20:48:46 -0300

    finish number styling

--- pages/home.vue
+++ pages/home.vue
@@ -3,25 +3,25 @@
         <NavBar>
             <AnonUser />
         </NavBar>
-        <div class="flex  w-[100vw] h-[90vh]">
+        <div class="flex   h-[86dvh]">
             <div class="dashboard grid grid-cols-[1fr_3fr] grid-rows-4 w-full h-full m-10 rounded-lg bg-customGray">
                 <div
-                    class="menu row-span-4  h-full w-full bg-customBlack rounded-lg text-customWhite grid grid-rows-4 ">
-                    <div class="home flex justify-center items-center text-center">
+                    class="menu row-span-4  h-full w-full bg-customBlack rounded-lg text-customWhite grid grid-rows-4 text-lg ">
+                    <div class="home flex justify-center items-center text-center ">
                         <div class="number ">1</div>
                         <span class="menuItem">Home</span>
                     </div>
-                    <div class="edit text-center flex justify-center items-center">
+                    <div class="edit  flex justify-center items-center">
                         <div class="number">2</div>
                         <span class="menuItem">Edit</span>
                     </div>
-                    <div class="create text-center flex justify-center items-center">
+                    <div class="create  flex justify-center items-center">
                         <div class="number">3</div>
                         <span class="menuItem">Create</span>
                     </div>
-                    <div class="bookmarked text-center flex justify-center items-center">
+                    <div class="bookmarked  flex justify-center items-center">
                         <div class="number">4</div>
-                        <span class="menuItem">Bookmarked</span>
+                        <span class="menuItem ml-9">Bookmarked</span>
                     </div>
                 </div>
                 <div class="content flex justify-center items-center">
@@ -40,4 +40,8 @@ import AnonUser from '~/components/AnonUser.vue';
 
 </script>
 
-<style scoped></style>
+<style>
+.number {
+    @apply p-5 px-[1.81rem] border-customGolden border-[0.5px] rounded-full absolute left-24;
+}
+</style>
