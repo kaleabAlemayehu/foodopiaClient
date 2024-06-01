636164b1a635e8031b5a8185c77a293a823df65a
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-05-31 22:47:52 -0300

    polishing filter direction with data-* attribute of flowbite

--- components/Filter.vue
+++ components/Filter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="filterBox  sticky right-20 top-20 py-5 px-16 w-[20%]">
 
-        <button id="dropdownHelperButton" data-dropdown-toggle="dropdownHelper"
+        <button id="dropdownHelperButton" data-dropdown-toggle="dropdownHelper" data-dropdown-placement="right"
             class="text-customBlack bg-customGray hover:bg-customGray focus:ring-4 focus:outline-none focus:ring-customGray font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-customGray dark:hover:bg-customGray dark:focus:ring-customGray"
             type="button">filters <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                 fill="none" viewBox="0 0 10 6">
@@ -18,7 +18,7 @@
                 <li>
 
 
-                    <button id="timeButton" data-dropdown-toggle="timeFilter"
+                    <button id="timeButton" data-dropdown-toggle="timeFilter" data-dropdown-placement="top-start"
                         class="text-customBlack bg-customGray hover:bg-customGray focus:ring-4 focus:outline-none focus:ring-customGray font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-customGray dark:hover:bg-customGray dark:focus:ring-customGray"
                         type="button">Time <svg class="w-2.5 h-2.5 ms-3" aria-hidden="false"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -41,6 +41,7 @@
 
 
                     <button id="ingredientButton" data-dropdown-toggle="ingredientFilter"
+                        data-dropdown-placement="bottom-start"
                         class="text-customBlack bg-customGray hover:bg-customGray focus:ring-4 focus:outline-none focus:ring-customGray font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-customGray dark:hover:bg-customGray dark:focus:ring-customGray"
                         type="button">Ingredients <svg class="w-2.5 h-2.5 ms-3" aria-hidden="false"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
