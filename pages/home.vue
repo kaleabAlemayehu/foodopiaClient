f20d8c8c92547eb78af1d039d131d3dddd1d95bb
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-06-01 21:21:26 -0300

    add mock style change behaviour on the click

--- pages/home.vue
+++ pages/home.vue
@@ -6,37 +6,25 @@
         <div class="flex   h-[86dvh]">
             <div class="dashboard grid grid-cols-[1fr_3fr] grid-rows-4 w-full h-full m-10 rounded-lg bg-customGray">
                 <div
-                    class="menu row-span-4  h-full w-full bg-customBlack rounded-lg text-customGolden grid grid-rows-4 text-lg  ">
-                    <div class="home flex justify-center items-center text-center">
-                        <div v-if="isCurrent"
-                            class=" text-4xl border-[0.5px] border-customGolden  p-3 px-[0.85rem] rounded-full text-customWhite absolute left-24 text-current">
-                            <Icon name="fa6-solid:check" />
-                        </div>
-                        <div v-else class="number ">1</div>
+                    class="menu row-span-4  h-full w-full bg-customBlack rounded-lg text-customWhite grid grid-rows-4 text-lg  ">
+                    <div class="home flex justify-center items-center text-center"
+                        :class="{ isCurrent: isCurrent == 1 ? true : false }" @click="changeCurrent">
+
                         <span class="menuItem">Home</span>
                     </div>
-                    <div class="edit  flex justify-center items-center">
-                        <div v-if="isCurrent"
-                            class=" text-4xl border-[0.5px] border-customGolden  p-3 px-[0.85rem] rounded-full text-customWhite absolute left-24 text-current">
-                            <Icon name="fa6-solid:check" />
-                        </div>
-                        <div v-else class="number ">2</div>
+                    <div class="edit  flex justify-center items-center"
+                        :class="{ isCurrent: isCurrent == 2 ? true : false }" @click="changeCurrent">
+
                         <span class="menuItem">Edit</span>
                     </div>
-                    <div class="create  flex justify-center items-center">
-                        <div v-if="isCurrent"
-                            class=" text-4xl border-[0.5px] border-customGolden  p-3 px-[0.85rem] rounded-full text-customWhite absolute left-24 text-current">
-                            <Icon name="fa6-solid:check" />
-                        </div>
-                        <div v-else class="number ">3</div>
+                    <div class="create  flex justify-center items-center"
+                        :class="{ isCurrent: isCurrent == 3 ? true : false }" @click="changeCurrent">
+
                         <span class="menuItem">Create</span>
                     </div>
-                    <div class="bookmarked  flex justify-center items-center">
-                        <div v-if="isCurrent"
-                            class=" text-4xl border-[0.5px] border-customGolden  p-3 px-[0.85rem] rounded-full text-customWhite absolute left-24 text-current">
-                            <Icon name="fa6-solid:check" />
-                        </div>
-                        <div v-else class="number ">4</div>
+                    <div class="bookmarked  flex justify-center items-center"
+                        :class="{ isCurrent: isCurrent == 4 ? true : false }" @click="changeCurrent">
+
                         <span class="menuItem ml-9">Bookmarked</span>
                     </div>
                 </div>
@@ -54,12 +42,18 @@
 import { ref } from "vue"
 import NavBar from '~/components/NavBar.vue';
 import AnonUser from '~/components/AnonUser.vue';
-const isCurrent = ref(true)
-
+const isCurrent = ref(1);
+const changeCurrent = (e) => {
+    isCurrent.value = 3;
+}
 </script>
 
 <style>
 .number {
     @apply p-5 px-[1.81rem] border-customGolden border-[0.5px] rounded-full absolute left-24;
+}
+
+.isCurrent {
+    @apply text-customGolden;
 }
 </style>
