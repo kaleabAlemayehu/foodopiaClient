c54b9b4d13fe3519542fae71428b57675b4abbd9
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-06-10 01:00:36 -0300

    change auth buttons

--- components/AnonUser.vue
+++ components/AnonUser.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="authBtns flex items-center justify-center ">
-        <NuxtLink to="/login">
-            <button class="btn">Login</button>
+        <NuxtLink to="/login" class="my-auto mx-auto">
+            <button type="button"
+                class="text-customWhite bg-customOrang hover:bg-[#f7734b] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5  mx-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">LogIn</button>
         </NuxtLink>
         <NuxtLink to="/signUp">
-            <button class="btn">SignUp</button>
+            <button type="button"
+                class="text-customWhite bg-customOrang hover:bg-[#f7734b] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5  mx-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">SignUp</button>
         </NuxtLink>
 
     </div>
