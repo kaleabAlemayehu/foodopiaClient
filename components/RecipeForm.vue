9b9fb5812857153a435cab8830080bf6341cdb63
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-06-24 15:35:26 -0300

    switching to 'Form' and 'Field' approach to include dynamic form valdiation

--- components/RecipeForm.vue
+++ components/RecipeForm.vue
@@ -1,50 +1,48 @@
 <template>
-    <form class="w-[60vw] mx-auto" @submit.prevent="onSubmit">
+    <Form class="w-[60vw] mx-auto" v-slot="{ errors, values }" @submit="onSubmit">
         <div class="my-5">
             <label for="title" class="block mb-2 text-sm font-medium text-customBlack dark:text-white">Title</label>
-            <input type="text" id="title" v-model="title" v-bind="titleAttrs"
+            <Field as="input" type="text" name='title' id="title" rules="required"
                 class="block w-full p-4 text-gray-900 border border-customGray rounded-lg bg-customWhite text-base focus:ring-customOrang focus:border-customOrang dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Title ...">
-            <div class="err">{{ errors.title }}</div>
+                placeholder="Title ..." />
+            <ErrorMessage class="err" name="title" />
         </div>
 
         <div class="mb-5">
             <label for="description"
                 class="block mb-2 text-sm font-medium text-customBlack dark:text-white">Description</label>
-            <textarea id="description" rows="4" v-model="description" v-bind="descriptionAttrs"
+            <Field as='textarea' name='description' rules="required" id="description" rows="4"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-customWhite rounded-lg border border-customWhite focus:ring-customOrang focus:border-customOrang dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-customOrang dark:focus:border-customOrang"
-                placeholder="Description ..."></textarea>
-            <div class="err">{{ errors.description }}</div>
+                placeholder="Description ..." />
+            <ErrorMessage name="description" class="err" />
         </div>
-        <div class="mb-5">
+        <div class=" mb-5">
             <label for="preparation" class="block mb-2 text-sm font-medium text-customBlack dark:text-white">preparation
                 time</label>
-            <input type="text" id="preparation" v-model="preparation" v-bind="preparationAttrs"
+            <Field as="input" type="text" id="preparation" name="preparation" rules="required|integer"
                 class="block w-full p-4 text-gray-900 border border-customGray rounded-lg bg-customWhite text-base focus:ring-customOrang focus:border-customOrang dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Preparation time in minutes ...">
-            <div class="err">{{ errors.preparation }}</div>
+                placeholder="Preparation time in minutes ..." />
+            <ErrorMessage name="preparation" class="err" />
         </div>
         <div class="mb-5">
             <label class="block mb-2 text-sm font-medium text-customBlack dark:text-white" for="featuredImage">Upload
                 Featured Picture</label>
-            <input
+            <Field as="input" name="featuredImage" rules="required|image|size:2500"
                 class="block w-full text-sm text-customBlack border border-gray-300 rounded-lg cursor-pointer bg-customWhite dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                aria-describedby="user_avatar_help" id="featuredImage" type="file" @change="handleFileChange"
-                v-validate="'image'">
+                aria-describedby="user_avatar_help" id="featuredImage" type="file" @change="handleFileChange" />
 
-            <div class="err">{{ errors.featureImage }}</div>
+            <ErrorMessage name="featuredImage" class="err" />
         </div>
         <div class="mb-5">
             <label class="block mb-2 text-sm font-medium text-customBlack dark:text-white" for="addtionalPic">Upload
                 Addtional Picture</label>
-            <input
+            <Field name='additionalImage' rules="image|size:2500"
                 class="block w-full text-sm text-customBlack border border-gray-300 rounded-lg cursor-pointer bg-customWhite dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                aria-describedby="addtionalOne" id="addtionalPic" type="file" @change="handleAddtional">
-            <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="addtionalOne">
-                Addtional Picture</div>
+                aria-describedby="addtionalOne" id="addtionalPic" type="file" @change="handleAddtional" />
+            <ErrorMessage class="err" name="additionalImage" />
         </div>
 
-        <div class="my-5">
+        <!-- <div class="my-5">
             <div class="my-5" v-for="ing, index in ings" :key="ing.name">
                 <label :for="ing.label"
                     class="block mb-2 text-sm font-medium text-customBlack dark:text-white">ingredient
@@ -81,138 +79,151 @@
                 class="text-white bg-customOrang hover:bg-customOrang focus:ring-4 focus:outline-none focus:ring-customWhitishOrange font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <Icon name="mi:remove" />
             </button>
-        </div>
+        </div> -->
         <button type="submit"
             class="text-white bg-customOrang hover:bg-customOrang focus:outline-none focus:ring-4 focus:ring-customWhitishOrange font-medium rounded-full text-sm px-5 py-2.5 text-center mx-auto mt-3 mb-5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Submit</button>
 
+        <h2>VeeValidate Info</h2>
+        <div>
+            <b>VeeValidate values:</b>
+            <pre>{{ values }}</pre>
+        </div>
+        <div>
+            <b>VeeValidate errors:</b>
+            <pre>{{ errors }}</pre>
+        </div>
 
-
-    </form>
+    </Form>
 </template>
 
-<script setup>
-const imageUpload = gql`mutation MyMutation($base64Str: String !, $name: String!) {
-  imageUpload(name: $name, base64Str: $base64Str) {
-    image_url
-  }
-}
-
-`
-import { reactive, ref } from 'vue';
-import { onMounted } from 'vue';
+<script>
+import { reactive, ref, onMounted, defineComponent } from 'vue';
 import { initFlowbite } from 'flowbite';
-import { useForm } from 'vee-validate';
+import { configure, ErrorMessage, Form, Field, defineRule } from 'vee-validate';
 import { useMutation } from '#imports';
-import { integer, required } from '@vee-validate/rules';
-const file = ref(null)
-const addtionalOne = ref(null)
-const fileUrl = ref(null)
-const addtionalUrl = ref(null)
-const base64Str = ref(null)
-const addtionalBase64Str = ref(null)
+import { integer, required, image, size, email } from '@vee-validate/rules';
+export default defineComponent({
+    name: "RecipeForm",
+    setup() {
 
 
-
-const { errors, defineField } = useForm(
-    {
-        validationSchema: {
-            title: required,
-            description: required,
-            preparation: required && integer,
+        const imageUpload = gql`mutation MyMutation($base64Str: String !, $name: String!) {
+                 imageUpload(name: $name, base64Str: $base64Str) {
+                image_url
         }
-    }
-)
+        }`
 
-const [title, titleAttrs] = defineField("title");
-const [description, descriptionAttrs] = defineField("description")
-const [preparation, preparationAttrs] = defineField("preparation")
+        const file = ref(null)
+        const addtionalOne = ref(null)
+        const fileUrl = ref(null)
+        const addtionalUrl = ref(null)
+        const base64Str = ref(null)
+        const addtionalBase64Str = ref(null)
 
-
-const uploadImage = async (image, base64Str) => {
-    try {
-        const variables = { name: image.name, base64Str: base64Str }
-        const { mutate } = useMutation(imageUpload, { variables })
-        const { data } = await mutate()
-        if (data && data.imageUpload && data.imageUpload.image_url) {
-            return data.imageUpload.image_url;
-        } else {
-            console.log('No image URL found in the response');
-            return null;
+        const uploadImage = async (image, base64Str) => {
+            try {
+                const variables = { name: image.name, base64Str: base64Str }
+                const { mutate } = useMutation(imageUpload, { variables })
+                const { data } = await mutate()
+                if (data && data.imageUpload && data.imageUpload.image_url) {
+                    return data.imageUpload.image_url;
+                } else {
+                    console.log('No image URL found in the response');
+                    return null;
+                }
+            } catch (er) {
+                console.log(er)
+                return null
+            }
         }
-    } catch (er) {
-        console.log(er)
-        return null
+
+
+        const handleFileChange = (event) => {
+            file.value = event.target.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(file.value);
+            reader.onload = () => {
+                base64Str.value = reader.result.split(',')[1]; // Remove the data URL part
+            };
+            reader.onerror = () => {
+                console.log('Unable to parse file');
+            };
+        }
+
+        const handleAddtional = (event) => {
+            addtionalOne.value = event.target.files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(addtionalOne.value);
+            reader.onload = () => {
+                addtionalBase64Str.value = reader.result.split(',')[1]; // Remove the data URL part
+            };
+            reader.onerror = () => {
+                console.log('Unable to parse file');
+            };
+        }
+
+
+        onMounted(() => {
+
+            initFlowbite();
+            configure({
+                validateOnInput: true,
+            });
+            defineRule("required", required)
+            defineRule("email", email)
+            defineRule("size", size)
+            defineRule("integer", integer)
+            defineRule("image", image)
+        })
+
+        const onSubmit = async (value) => {
+            alert(JSON.stringify(value, null, " "))
+        }
+        // const ings = ref([{
+        //     name: "ing1",
+        //     label: "Ing 1",
+        //     rules: { required }
+        // }])
+        // const ingValues = reactive({
+        //     ing1: ''
+        // })
+
+        // const addIng = () => {
+        //     const newIndex = ings.value.length + 1;
+        //     const ingName = `ing${newIndex}`;
+        //     ings.value.push({
+        //         name: ingName,
+        //         label: `Ing ${newIndex}`,
+        //         rules: { required }
+        //     })
+        //     ingValues[ingName] = '';
+        //     defineField(ingName, ingValues[ingName], { required })
+        // }
+        // ings.value.forEach(ing => {
+        //     defineField(ing.name, ingValues[ing.name], ing.rules)
+        // })
+
+        // const steps = reactive([1, 2, 3])
+        // const remove = (value) => {
+        //     value.pop()
+        // }
+        // const add = (value) => {
+        //     value.push(value.length + 1)
+        // }
+
+        return {
+            onSubmit,
+            handleAddtional,
+            handleFileChange,
+        }
+    },
+    components: {
+        Form,
+        Field,
+        ErrorMessage,
     }
-}
+});
 
-
-const handleFileChange = (event) => {
-    file.value = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file.value);
-    reader.onload = () => {
-        base64Str.value = reader.result.split(',')[1]; // Remove the data URL part
-    };
-    reader.onerror = () => {
-        console.log('Unable to parse file');
-    };
-}
-
-const handleAddtional = (event) => {
-    addtionalOne.value = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(addtionalOne.value);
-    reader.onload = () => {
-        addtionalBase64Str.value = reader.result.split(',')[1]; // Remove the data URL part
-    };
-    reader.onerror = () => {
-        console.log('Unable to parse file');
-    };
-}
-
-
-onMounted(() => {
-
-    initFlowbite();
-})
-
-const onSubmit = async () => {
-    fileUrl.value = await uploadImage(file.value, base64Str.value)
-    addtionalUrl.value = await uploadImage(addtionalOne.value, addtionalBase64Str.value)
-    console.log(fileUrl.value);
-    console.log(addtionalUrl.value);
-}
-const ings = ref([{
-    name: "ing1",
-    label: "Ing 1",
-    rules: { required }
-}])
-const ingValues = reactive({
-    ing1: ''
-})
-
-const addIng = () => {
-    const newIndex = ings.value.length + 1;
-    const ingName = `ing${newIndex}`;
-    ings.value.push({
-        name: ingName,
-        label: `Ing ${newIndex}`,
-        rules: { required }
-    })
-    ingValues[ingName] = '';
-    defineField(ingName, ingValues[ingName], { required })
-}
-ings.value.forEach(ing => {
-    defineField(ing.name, ingValues[ing.name], ing.rules)
-})
-
-const steps = reactive([1, 2, 3])
-const remove = (value) => {
-    value.pop()
-}
-const add = (value) => {
-    value.push(value.length + 1)
-}
 </script>
 
 <style>
