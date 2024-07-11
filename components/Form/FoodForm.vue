5996ce9d066b3b8c93d331dba7bf38087c439ab3
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-11 11:51:37 -0400

    adding add button for the ingredients dynamic value

--- components/Form/FoodForm.vue
+++ components/Form/FoodForm.vue
@@ -78,10 +78,13 @@
 
 
                     </div>
-
-
                 </div>
-
+                <button type="button"
+                    @click="ingredients.push({ name: 'test 1', amount: '2cups', id: ingredients.length })"
+                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                    <Add class="inline w-6 h-6" /> &nbsp; &nbsp;
+                    Add More Ingredients
+                </button>
             </div>
 
         </form>
@@ -102,6 +105,7 @@ instruction
 -->
 <script setup>
 import { ref } from 'vue';
+import Add from '../icons/Add.vue';
 const ingredients = ref([{
     id: 1,
     name: "test 1",
