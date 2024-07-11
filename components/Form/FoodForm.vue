d177e349432631f760f555ec0e6545ef189f38df
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-11 11:29:17 -0400

    adding catagory to the form

--- components/Form/FoodForm.vue
+++ components/Form/FoodForm.vue
@@ -38,6 +38,25 @@
                 <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A picture is
                     useful to confirm your are logged into your account</div>
             </div>
+            <div class="mb-5">
+                <label for="catagories" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                    Recipe Catagories</label>
+                <select id="catagories"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    <option>Breakfast</option>
+                    <option>Lunch</option>
+                    <option>Dinner</option>
+                    <option>Dessert</option>
+                    <option>Drink</option>
+                    <option>Snacks</option>
+                    <option>Seasonal</option>
+
+                </select>
+            </div>
+            <div class="mb-5">
+
+            </div>
         </form>
 
     </div>
