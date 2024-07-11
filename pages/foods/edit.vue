1d567685d6567ce97c1ccad6bf76f528d86cbff7
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-11 16:42:57 -0400

    adding margin for the edit page

--- pages/foods/edit.vue
+++ pages/foods/edit.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="my-16 max-w-screen-xl mx-auto px-6">
+    <div class="mt-24 mb-36 max-w-screen-xl mx-auto px-6">
         <h2 class="text-center text-2xl">Edit A Recipe</h2>
         <FoodForm class="self-start" />
-
     </div>
 </template>
 
