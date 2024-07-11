57da3cd3e0a21b739d65099ac668d24f5354c953
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-11 11:04:31 -0400

    add Title, desc, prep time, to foodForm and creating edit route

---
+++ components/Form/FoodForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <form class="max-w-2xl mx-auto my-16">
+
+            <div class="mb-5">
+                <label for="base-input"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                <input type="text" id="base-input"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Recipe Title">
+
+
+            </div>
+            <div class="mb-5">
+
+
+                <label for="message"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <textarea id="message" rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Describe The Recipe..."></textarea>
+            </div>
+            <div class="mb-5">
+                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preparation
+                    Time</label>
+                <input type="number" id="base-input"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter Preparation Time">
+
+
+            </div>
+            <div class="mb-5">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload
+                    file</label>
+                <input
+                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    aria-describedby="user_avatar_help" id="user_avatar" multiple type="file">
+                <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A picture is
+                    useful to confirm your are logged into your account</div>
+            </div>
+        </form>
+
+    </div>
+</template>
+<!-- 
+title
+description
+preparation time
+images
+catagories
+ingredients
+instruction
+
+
+
+-->
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ pages/foods/edit.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="my-16 max-w-screen-xl mx-auto px-6">
+        <h2 class="text-center text-xl">Edit A Recipe</h2>
+        <FoodForm class="self-start" />
+
+    </div>
+</template>
+
+<script setup>
+import FoodForm from '~/components/Form/FoodForm.vue';
+
+</script>
+
+<style scoped></style>
