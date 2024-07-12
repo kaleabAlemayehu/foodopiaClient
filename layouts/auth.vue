c3b2b51dadf79cff57e7afae60a6ec242278828a
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-12 03:20:05 -0400

    add auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center items-center w-screen h-screen">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ pages/auth/signup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <SignupForm />
+
+    </div>
+</template>
+
+<script setup>
+import SignupForm from '~/components/Form/SignupForm.vue';
+import { definePageMeta } from '#imports';
+definePageMeta({
+    layout: "auth"
+})
+</script>
+
+<style scoped></style>
