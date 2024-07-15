5658d8b12b926c67e9dd6c5e8439254454f86d33
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-15 10:54:08 -0400

    add validation for signupform

--- components/Form/SignupForm.vue
+++ components/Form/SignupForm.vue
@@ -13,47 +13,52 @@
                             class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create an account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <Form class="space-y-4 md:space-y-6" action="#" @submit="onSubmit">
                             <div>
                                 <label for="username"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                                <input type="text" name="username" id="username"
+                                <Field as="input" type="text" name="username" id="username" rules="required"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Enter Your Username..." required="">
+                                    placeholder="Enter Your Username..." required="" />
+                                <ErrorMessage name="username" class="err" />
                             </div>
                             <div>
                                 <label for="email"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                     email</label>
-                                <input type="email" name="email" id="email"
+                                <Field as="input" rules="email|required" type="email" name="email" id="email"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Enter Your Email..." required="">
+                                    placeholder="Enter Your Email..." required="" />
+                                <ErrorMessage name="email" class="err" />
                             </div>
                             <div>
                                 <label for="password"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                <Field as="input" rules="required|min:8|max:16" type="password" name="password"
+                                    id="password" placeholder="••••••••"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required="">
+                                    required="" />
+                                <ErrorMessage name="password" class="err" />
                             </div>
                             <div>
                                 <label for="confirm-password"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
                                     password</label>
-                                <input type="password" name="confirm-password" id="confirm-password"
-                                    placeholder="••••••••"
+                                <Field type="password" name="confirm-password" id="confirm-password"
+                                    placeholder="••••••••" rules="required|confirmed:@password"
                                     class="bg-gray-50 mb-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required="">
+                                    required="" />
+                                <ErrorMessage name="confirm-password" class="err" />
                             </div>
                             <button type="submit"
-                                class="w-full  bg-red-600 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
+                                class="w-full  text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Create
                                 an account</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400 py-4 ">
                                 Already have an account? <a href="/auth/signin"
                                     class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
                                     here</a>
                             </p>
-                        </form>
+                        </Form>
                     </div>
                 </div>
             </div>
@@ -63,7 +68,12 @@
 </template>
 
 <script setup>
+import { Field, Form, ErrorMessage } from 'vee-validate';
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.err {
+    @apply text-xs text-red-600 dark:text-gray-300;
+}
+</style>
--- plugins/rules.js
+++ plugins/rules.js
@@ -10,6 +10,7 @@ import {
   alpha_spaces,
   min,
   max,
+  confirmed,
 } from "@vee-validate/rules";
 export default defineNuxtPlugin({});
 // Define rules
@@ -23,6 +24,7 @@ defineRule("image", image);
 defineRule("size", size);
 defineRule("min", min);
 defineRule("max", max);
+defineRule("confirmed", confirmed);
 // Configure custom messages
 configure({
   validateOnInput: true,
@@ -31,13 +33,14 @@ configure({
       required: `This field is required.`,
       email: `${context.field} must be a valid email address.`,
       size: `${context.field} is must be less than 1.5MB.`,
-      image: `This field is must be an image.`,
+      image: `This field must be an image.`,
       integer: `${context.field} is must be an integer.`,
       alpha_dash: `${context.field} is must content only alphabets and dashes.`,
       alpha_num: `${context.field} is must content only alphabets and numbers.`,
       alpha_spaces: `${context.field} is must content only alphabets and spaces.`,
       min: `${context.field} is must be at least ${context.rule.params} characters.`,
       max: `${context.field} is must be at most ${context.rule.params} characters.`,
+      confirmed: `The passwords doesn't match.`,
     };
     return messages[context.rule.name]
       ? messages[context.rule.name]
