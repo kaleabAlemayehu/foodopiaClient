8412aba0d9395b40187da5dac85de7e01b2c9c08
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-15 07:49:45 -0400

    add rules and validation on dynamic fields

--- components/Form/FoodForm.vue
+++ components/Form/FoodForm.vue
@@ -26,64 +26,73 @@
 
 
             <div class="mb-5">
-
-                <div v-for="ingredient, index in ingredients" :key="ingredient.id"
-                    class=" mb-5 flex justify-evenly items-center gap-3 transition duration-700">
-                    <div class="">
-                        <label :for="ingredient.name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ingredients </label>
-                        <input type="text" :id="ingredient.name"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            :placeholder="`Ingredient ${index + 1}`">
-
-
-                    </div>
-                    <div class="">
-                        <label :for="ingredient.amount"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount </label>
-                        <div class="flex gap-4">
-
-                            <input type="text" :id="ingredient.amount"
+                <FieldArray name="ingredients" v-slot="{ fields, push, remove }">
+                    <div v-for="entry, index in fields" :key="index"
+                        class=" mb-5 flex justify-evenly items-center gap-3 transition duration-700">
+                        <div class="">
+                            <label :for="`ingredient${index}.name`"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ingredient {{ index
+                                }}
+                            </label>
+                            <Field type="text" :id="`ingredient${index}.name`" :name="`ingredients[${index}].name`"
+                                as="input" rules="required"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                :placeholder="`Amount ${index + 1}`">
-                            <Cross class="inline text-primary w-8 h-8 my-auto " @click="ingredients.pop()" />
+                                :placeholder="`Ingredient ${index + 1}`" />
+                            <ErrorMessage :name="`ingredients[${index}].name`" class="err" />
                         </div>
+                        <div class="">
+                            <label :for="`ingredients[${index}].amount`"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount
+                            </label>
+                            <div class="flex gap-4">
+
+                                <Field as="input" :name="`ingredients[${index}].amount`" type="text"
+                                    :id="`ingredients[${index}].amount`" rules="required"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    :placeholder="`Amount ${index + 1}`" />
+                                <Cross class="inline text-primary w-8 h-8 my-auto " @click="remove(index)" />
+                            </div>
+                            <ErrorMessage :name="`ingredients[${index}].amount`" class="err" />
 
 
+                        </div>
                     </div>
-                </div>
-                <button type="button"
-                    @click="ingredients.push({ name: 'test 1', amount: '2cups', id: ingredients.length })"
-                    class="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                    <Add class="inline w-6 h-6" /> &nbsp; &nbsp;
-                    Add Ingredient
-                </button>
+                    <button type="button" @click="push({})"
+                        class="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                        <Add class="inline w-6 h-6" /> &nbsp; &nbsp;
+                        Add Ingredient
+                    </button>
+                </FieldArray>
             </div>
             <div class="mb-5">
-                <div class="flex " v-for="instruction, index in instructions" :key="index">
-                    <div class="w-full mb-5">
-                        <label :for="index"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instruction
-                            {{ index
-                                + 1
-                            }}</label>
-                        <div class="flex gap-4">
+                <FieldArray name="instructions" v-slot="{ fields, push, remove }">
 
-                            <input type="text" :id="index"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                :placeholder="`Instruction ${index + 1}`">
-                            <Cross class="inline text-primary w-8 h-8 my-auto " @click="instructions.pop()" />
+                    <div class="flex " v-for="entry, index in fields" :key="index">
+                        <div class="w-full mb-5">
+                            <label :for="`instructions[${index}].name`"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instruction
+                                {{ index
+                                    + 1
+                                }}</label>
+                            <div class="flex gap-4">
+
+                                <Field type="text" :id="`instructions[${index}].name`" as="textarea"
+                                    :name="`instructions[${index}].name`"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    :placeholder="`Instruction ${index + 1}`" />
+                                <Cross class="inline text-primary w-8 h-8 my-auto " @click="remove(index)" />
+                            </div>
+                            <ErrorMessage :name="`instructions[${index}].name`" class="err" />
+
                         </div>
 
-
                     </div>
-
-                </div>
-                <button type="button" @click="instructions.push('')"
-                    class="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                    <Add class="inline w-6 h-6" /> &nbsp; &nbsp;
-                    Add Instruction
-                </button>
+                    <button type="button" @click="push({})"
+                        class="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                        <Add class="inline w-6 h-6" /> &nbsp; &nbsp;
+                        Add Instruction
+                    </button>
+                </FieldArray>
             </div>
             <div class="mb-5">
                 <label for="preparationTime"
@@ -136,7 +145,7 @@
 import { ref } from 'vue';
 import Add from '../icons/Add.vue';
 import Cross from '../icons/Cross.vue';
-import { configure, Form, Field, ErrorMessage, defineRule } from 'vee-validate';
+import { configure, Form, Field, ErrorMessage, defineRule, FieldArray } from 'vee-validate';
 import { alpha_spaces, alpha_num, alpha_dash, required, integer } from '@vee-validate/rules';
 
 onMounted(() => {
