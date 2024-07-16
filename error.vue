2877917e190c47f480397af48660a013f04f7207
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-16 02:56:49 -0400

    add 404 not found custom page

--- error.vue
+++ error.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="mt-24 mb-36 max-w-screen-xl mx-auto px-6">
+    <Navbar />
+    <div class=" mt-12 max-w-screen-xl mx-auto px-6">
         <client-only>
             <BackRoute />
             <Vue3Lottie animationLink="https://lottie.host/5b648ae2-e6a5-49bc-ae77-5b7a2e920f03/fBIdIS5FFc.json"
-                :height="800" :width="800" />
+                :height="600" :width="800" />
         </client-only>
     </div>
+    <Footer />
 </template>
 
 <script setup>
 import BackRoute from "./components/Food/BackRoute.vue"
+import Navbar from "./components/Navbar/Navbar.vue"
+import Footer from "./components/Footer/Footer.vue"
 
 import { Vue3Lottie } from 'vue3-lottie'
 
