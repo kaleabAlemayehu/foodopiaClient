f65f7967f954a30f7dcb242ef40caf7064a71a41
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-16 02:38:22 -0400

    add 404 lottie file

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mt-24 mb-36 max-w-screen-xl mx-auto px-6">
+        <client-only>
+            <BackRoute />
+            <Vue3Lottie animationLink="https://lottie.host/5b648ae2-e6a5-49bc-ae77-5b7a2e920f03/fBIdIS5FFc.json"
+                :height="800" :width="800" />
+        </client-only>
+    </div>
+</template>
+
+<script setup>
+import BackRoute from "./components/Food/BackRoute.vue"
+
+import { Vue3Lottie } from 'vue3-lottie'
+
+</script>
+
+<style scoped></style>
---
+++ layouts/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Navbar />
+        <slot />
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+
+import Footer from "../components/Footer/Footer.vue"
+import Navbar from '../components/Navbar/Navbar.vue';
+
+
+</script>
+
+<style scoped></style>
